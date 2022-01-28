7593dd389236835d59c32cbed1bb5485a57db3d0
barbosdiego <diego.gutemberg@hotmail.com>
2022-01-28 15:30:05 -0300

    js-adicionado

---
+++ cardapio/script.js
@@ -0,0 +1,148 @@
+const menu = [
+  {
+    id: 1,
+    title: "buttermilk pancakes",
+    category: "breakfast",
+    price: 15.99,
+    img: "./images/item-1.jpeg",
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+  },
+  {
+    id: 2,
+    title: "diner double",
+    category: "lunch",
+    price: 13.99,
+    img: "./images/item-2.jpeg",
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+  },
+  {
+    id: 3,
+    title: "godzilla milkshake",
+    category: "shakes",
+    price: 6.99,
+    img: "./images/item-3.jpeg",
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 4,
+    title: "country delight",
+    category: "breakfast",
+    price: 20.99,
+    img: "./images/item-4.jpeg",
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+  },
+  {
+    id: 5,
+    title: "egg attack",
+    category: "lunch",
+    price: 22.99,
+    img: "./images/item-5.jpeg",
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 6,
+    title: "oreo dream",
+    category: "shakes",
+    price: 18.99,
+    img: "./images/item-6.jpeg",
+    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+  },
+  {
+    id: 7,
+    title: "bacon overflow",
+    category: "breakfast",
+    price: 8.99,
+    img: "./images/item-7.jpeg",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },
+  {
+    id: 8,
+    title: "american classic",
+    category: "lunch",
+    price: 12.99,
+    img: "./images/item-8.jpeg",
+    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+  },
+  {
+    id: 9,
+    title: "quarantine buddy",
+    category: "shakes",
+    price: 16.99,
+    img: "./images/item-9.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 10,
+    title: "teste",
+    category: "dinner",
+    price: 32.59,
+    img: "./images/item-10.jpeg",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird.`,
+  }
+];
+// const buttons = document.querySelectorAll(".btn");
+const container = document.querySelector(".cardapio");
+const btnContainer = document.querySelector('.filter');
+
+
+menu.forEach((item) => createItem(item));
+btnContainer.addEventListener('click', (e) => {
+  container.innerHTML = ''
+  const category = e.target.id;
+  filter(category)
+})
+
+// buttons.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     container.innerHTML = "";
+//     const category = e.target.id;
+//     filter(category);
+//   });
+// });
+
+function filter(category) {
+  const res = menu.filter((item) => {
+    if (item.category === category) {
+      return item;
+    } else if (category === "all") {
+      return item;
+    }
+  });
+  res.forEach((item) => createItem(item));
+}
+
+function createItem(item) {
+  const div = document.createElement("div");
+  div.classList.add("cardapio__card");
+  div.innerHTML = `
+    <img src="${item.img}" alt="${item.title}">
+    <div>
+      <div class="card__info">
+        <h3 class="card__info-title">${item.title}</h3>
+        <span class="card__info-price">$${item.price}</span>
+      </div>
+      
+      <p class="description">${item.desc}</p>
+    </div>
+  `;
+  container.appendChild(div);
+}
+
+const btnArray = menu
+.map((item) => item.category)
+.reduce((acc, item) => {
+  if(!acc.includes(item)){
+    acc.push(item)
+  }
+  return acc
+}, ['all'])
+
+btnArray.forEach((btn) => createButton(btn));
+
+function createButton(btn) {
+  const button = document.createElement("button");
+  button.classList.add("btn");
+  button.id = btn;
+  button.innerText = btn;
+  btnContainer.appendChild(button);
+}
