946882694bb2c42b51fe739b209a5fc8c15dab39
barbosdiego <diego.gutemberg@hotmail.com>
2022-02-04 14:49:04 -0300

    header e js

---
+++ plantshop - figma/script.js
@@ -0,0 +1,11 @@
+const menuBtn = document.querySelector('.btn-menu');
+const closeBtn = document.querySelector('.close-btn');
+const nav = document.querySelector('nav');
+
+menuBtn.addEventListener('click', () => {
+  nav.classList.add('active');
+})
+
+closeBtn.addEventListener('click', () => {
+  nav.classList.remove('active');
+})
