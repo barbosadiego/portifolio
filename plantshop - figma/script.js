11d9e786e39a5370bb7683dd36cc7c3a935f63b6
barbosdiego <diego.gutemberg@hotmail.com>
2022-02-05 12:27:41 -0300

    js hero

--- plantshop - figma/script.js
+++ plantshop - figma/script.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector(".btn-menu");
 const closeBtn = document.querySelector(".close-btn");
 const nav = document.querySelector("nav");
+const itemsHero = document.querySelectorAll(".section-1 .carrosel .item");
 
 menuBtn.addEventListener("click", () => {
   nav.classList.add("active");
@@ -13,3 +14,41 @@ closeBtn.addEventListener("click", () => {
 window.onscroll = () => {
   nav.classList.remove("active");
 };
+
+for (let i = 0; i < itemsHero.length; i++) {
+  createDot(document.querySelector(".dots-container"));
+}
+
+function createDot(container) {
+  const div = document.createElement("div");
+  div.classList.add("dot");
+  container.appendChild(div);
+}
+
+const dotsHero = document.querySelectorAll(".section-1 .dot");
+
+itemsHero[0].classList.add("active");
+dotsHero[0].classList.add("active");
+
+dotsHero.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    activeItem(index);
+  });
+});
+
+function activeItem(index) {
+  itemsHero.forEach((item) => item.classList.remove("active"));
+  dotsHero.forEach((item) => item.classList.remove("active"));
+
+  itemsHero[index].classList.add("active");
+  dotsHero[index].classList.add("active");
+}
+
+let index = 0;
+setInterval(function () {
+  activeItem(index);
+  index++;
+  if (index === itemsHero.length) {
+    index = 0;
+  }
+}, 3000);
