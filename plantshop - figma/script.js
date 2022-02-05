d8c7e45014c8b46eedb2f86b965605c8a25debb6
barbosdiego <diego.gutemberg@hotmail.com>
2022-02-05 16:10:34 -0300

    scroll produtos

--- plantshop - figma/script.js
+++ plantshop - figma/script.js
@@ -3,6 +3,8 @@ const closeBtn = document.querySelector(".close-btn");
 const nav = document.querySelector("nav");
 const itemsHero = document.querySelectorAll(".section-1 .carrosel .item");
 
+// menu mobile
+
 menuBtn.addEventListener("click", () => {
   nav.classList.add("active");
 });
@@ -15,26 +17,21 @@ window.onscroll = () => {
   nav.classList.remove("active");
 };
 
+// hero slider
+
 for (let i = 0; i < itemsHero.length; i++) {
   createDot(document.querySelector(".dots-container"));
 }
+
+const dotsHero = document.querySelectorAll(".section-1 .dot");
+itemsHero[0].classList.add("active");
+dotsHero[0].classList.add("active");
 
 function createDot(container) {
   const div = document.createElement("div");
   div.classList.add("dot");
   container.appendChild(div);
 }
-
-const dotsHero = document.querySelectorAll(".section-1 .dot");
-
-itemsHero[0].classList.add("active");
-dotsHero[0].classList.add("active");
-
-dotsHero.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    activeItem(index);
-  });
-});
 
 function activeItem(index) {
   itemsHero.forEach((item) => item.classList.remove("active"));
@@ -44,7 +41,13 @@ function activeItem(index) {
   dotsHero[index].classList.add("active");
 }
 
-let index = 0;
+dotsHero.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    activeItem(index);
+  });
+});
+
+let index = 1;
 setInterval(function () {
   activeItem(index);
   index++;
@@ -52,3 +55,17 @@ setInterval(function () {
     index = 0;
   }
 }, 3000);
+
+// product slider
+
+const leftBtn = document.querySelector('.section-5 .left');
+const rightBtn = document.querySelector('.section-5 .right');
+const cardContainer = document.querySelector('.section-5 .card-container');
+
+leftBtn.addEventListener('click', ()=>{
+  cardContainer.scrollLeft += 320
+})
+
+rightBtn.addEventListener('click', ()=>{
+  cardContainer.scrollLeft += -320
+})
