8ad085b1820ff1ec761f4e06f513dd7d19c35ea7
barbosdiego <diego.gutemberg@hotmail.com>
2022-02-10 08:41:52 -0300

    debounce simples

--- repair-site/js/main.js
+++ repair-site/js/main.js
@@ -6,14 +6,22 @@ closeBtn.addEventListener('click', () => {
   closeBtn.classList.toggle('fa-times')
 })
 
-window.onscroll = () => {
-  nav.classList.remove('active');
-  closeBtn.classList.add('fa-bars');
-  closeBtn.classList.remove('fa-times');
 
-  if (window.scrollY > 60){
-    document.querySelector('.header .navbar').classList.add('active');
-  } else {
-    document.querySelector('.header .navbar').classList.remove('active');
-  }
+let time = null
+window.onscroll = () => {
+  // debounce
+  clearTimeout(time)
+  time = setTimeout(function(){
+    nav.classList.remove('active');
+    closeBtn.classList.add('fa-bars');
+    closeBtn.classList.remove('fa-times');
+    // console.log('ok')
+
+    if (window.scrollY > 60){
+      document.querySelector('.header .navbar').classList.add('active');
+    } else {
+      document.querySelector('.header .navbar').classList.remove('active');
+    }
+    // console.log('oi')
+  }, 50)
 }
